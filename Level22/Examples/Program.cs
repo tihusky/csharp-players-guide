e3c0b09b8f086fa48495ec3e9667502db85d49d4
Mirko Förster <mfoerster.hb@gmail.com>
2023-08-19 11:18:26 +0200

    Finished level 22

---
+++ Level22/Examples/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter your name: ");
+string? name = Console.ReadLine()?.Trim();
+
+if (string.IsNullOrEmpty(name)) {
+    name = "Anonymous";
+}
+
+Console.WriteLine($"Hey there, {name}!");
