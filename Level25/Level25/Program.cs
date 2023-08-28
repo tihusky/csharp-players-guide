aa7272051ad369e7732a755ce6496d18b34f8ac2
Mirko Förster <mfoerster.hb@gmail.com>
2023-08-28 12:53:30 +0200

    Finished level 25

---
+++ Level25/Level25/Program.cs
@@ -0,0 +1,140 @@
+﻿void Test1()
+{
+    object thing = new object();
+
+    Console.WriteLine(thing);
+
+    object a = new object();
+    object b = a;
+    object c = new object();
+
+    Console.WriteLine(a.Equals(b));
+    Console.WriteLine(a.Equals(c));
+
+    Point p1 = new Point(4, 2);
+    Point p2 = p1;
+
+    Console.WriteLine(p1.ToString());
+    Console.WriteLine(p1.Equals(p2));
+
+    object p3 = new Point(2, 4);
+
+    Console.WriteLine(p3.ToString());
+
+    // Because the compiler only knows p3 holds an object, more specific members from the Point class can't be accessed
+    // Console.WriteLine(p3.X);
+}
+
+void Test2()
+{
+    GameObject thing = new Asteroid();
+    
+    // Even though we can see this code is wrong the compiler allows it. It will lead to a crash!
+    var playerShip = (Ship)thing;
+}
+
+void Test3()
+{
+    GameObject thing = new Ship();
+
+    if (thing.GetType() == typeof(Ship))
+    {
+        ((Ship)thing).Shoot();
+    }
+
+    Asteroid? asteroid = thing as Asteroid;
+
+    if (asteroid == null)
+    {
+        Console.WriteLine("The thing variable doesn't hold an Asteroid object.");
+    }
+
+    if (thing is Ship playerShip)
+    {
+        Console.WriteLine($"The player's health is {playerShip.Health}.");
+    }
+}
+
+Test3();
+
+/* Type Definitions */
+
+public class Point
+{
+    public float X { get; }
+    public float Y { get; }
+
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+}
+
+public class GameObject
+{
+    public float PositionX { get; set; }
+    public float PositionY { get; set; }
+    public float VelocityX { get; set; }
+    public float VelocityY { get; set; }
+    
+    public GameObject()
+    {
+        Console.WriteLine("In the GameObject() constructor");
+    }
+
+    public GameObject(float positionX, float positionY)
+    {
+        Console.WriteLine("In the GameObject(float, float) constructor");
+        
+        PositionX = positionX;
+        PositionY = positionY;
+    }
+
+    public void Update()
+    {
+        PositionX += VelocityX;
+        PositionY += VelocityY;
+    }
+}
+
+public class Asteroid : GameObject
+{
+    public int Size { get; }
+    public float RotationAngle { get; }
+
+    public Asteroid()
+    {
+        Console.WriteLine("In the Asteroid() constructor");
+    }
+
+    public Asteroid(int size, float rotationAngle)
+    {
+        Console.WriteLine("In the Asteroid(int, float) constructor");
+        
+        Size = size;
+        RotationAngle = rotationAngle;
+    }
+}
+
+public class Ship : GameObject
+{
+    public int Health { get; set; }
+
+    public Ship() : this(100)
+    {
+        Console.WriteLine("In the Ship() constructor");
+    }
+    
+    public Ship(int health)
+    {
+        Console.WriteLine("In the Ship(int) constructor");
+        
+        Health = health;
+    }
+
+    public void Shoot()
+    {
+        Console.WriteLine("The player has fired a shot.");
+    }
+}
---
+++ Level25/PackingInventory/Program.cs
@@ -0,0 +1,145 @@
+﻿void WriteLineColored(string text, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.WriteLine(text);
+    Console.ResetColor();
+}
+
+int GetIntBetween(int min, int max, string prompt)
+{
+    while (true)
+    {
+        Console.Write($"{prompt} ");
+
+        bool isParseable = int.TryParse(Console.ReadLine(), out int number);
+
+        if (isParseable && number >= min && number <= max)
+            return number;
+
+        WriteLineColored("Invalid input.", ConsoleColor.Red);
+    }
+}
+
+Pack pack = new Pack(10, 25.0f, 25.0f);
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine($"{pack.Stats}\n");
+    Console.WriteLine("""
+                      What kind of item do you want to add?
+                      1) Arrow
+                      2) Bow
+                      3) Rope
+                      4) Water
+                      5) Food Ration
+                      6) Sword
+                      """);
+
+    int userSelection = GetIntBetween(1, 6, ">");
+
+    InventoryItem item = userSelection switch
+    {
+        1 => new Arrow(),
+        2 => new Bow(),
+        3 => new Rope(),
+        4 => new Water(),
+        5 => new FoodRation(),
+        6 => new Sword()
+    };
+
+    if (pack.Add(item))
+    {
+        WriteLineColored("The item was added successfully.", ConsoleColor.Green);
+    }
+    else
+    {
+        WriteLineColored("The item could not be added.", ConsoleColor.Red);
+    }
+
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey();
+}
+
+/***** Type Definitions *****/
+
+public class Pack
+{
+    private InventoryItem[] _items;
+    private int _itemCount = 0;
+    private readonly float _weightLimit;
+    private float _currentWeight = 0.0f;
+    private readonly float _volumeLimit;
+    private float _currentVolume = 0.0f;
+
+    public string CurrentItemCount => $"{_itemCount}/{_items.Length}";
+    public string CurrentWeight => $"{_currentWeight:0.00}/{_weightLimit:0.00}";
+    public string CurrentVolume => $"{_currentVolume:0.00}/{_volumeLimit:0.00}";
+    public string Stats => $"Items: {CurrentItemCount} Weight: {CurrentWeight} Volume: {CurrentVolume}";
+
+    public Pack(int itemLimit, float weightLimit, float volumeLimit)
+    {
+        _items = new InventoryItem[itemLimit];
+        _weightLimit = weightLimit;
+        _volumeLimit = volumeLimit;
+    }
+
+    public bool Add(InventoryItem item)
+    {
+        int newItemCount = _itemCount + 1;
+        float newWeight = _currentWeight + item.Weight;
+        float newVolume = _currentVolume + item.Volume;
+
+        if (newItemCount > _items.Length || newWeight > _weightLimit || newVolume > _volumeLimit)
+            return false;
+
+        _items[_itemCount] = item;
+        _itemCount++;
+        _currentWeight = newWeight;
+        _currentVolume = newVolume;
+
+        return true;
+    }
+}
+
+public class InventoryItem
+{
+    public float Weight { get; }
+    public float Volume { get; }
+
+    protected InventoryItem(float weight, float volume)
+    {
+        Weight = weight;
+        Volume = volume;
+    }
+}
+
+public class Arrow : InventoryItem
+{
+    public Arrow() : base(0.1f, 0.05f) {}
+}
+
+public class Bow : InventoryItem
+{
+    public Bow() : base(1.0f, 4.0f) {}
+}
+
+public class Rope : InventoryItem
+{
+    public Rope() : base(1.0f, 1.5f) {}
+}
+
+public class Water : InventoryItem
+{
+    public Water() : base(2.0f, 3.0f) {}
+}
+
+public class FoodRation : InventoryItem
+{
+    public FoodRation() : base(1.0f, 0.5f) {}
+}
+
+public class Sword : InventoryItem
+{
+    public Sword() : base(5.0f, 3.0f) {}
+}
