8ea060ac093c087976c4c614f77769df8545eb6f
Mirko Förster <mfoerster.hb@proton.me>
2023-08-31 11:19:27 +0200

    Finished level 28

---
+++ Level28/Level28/Program.cs
@@ -0,0 +1,24 @@
+﻿Point p1 = new Point(4, 2);
+Point p2 = new Point(); // Compiler still generates a parameterless constructor
+Point p3 = p2;
+
+p3.X = 2;
+p3.Y = 4;
+
+Console.WriteLine($"p1: ({p1.X}, {p1.Y})");
+Console.WriteLine($"p2: ({p2.X}, {p2.Y})");
+Console.WriteLine($"p3: ({p3.X}, {p3.Y})");
+
+/***** Type Definitions *****/
+
+public struct Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ Level28/RoomCoordinates/Program.cs
@@ -0,0 +1,38 @@
+﻿void PrintAdjacency(Coordinate a, Coordinate b)
+{
+    Console.WriteLine(a.AdjacentTo(b) ? $"{a} is adjacent to {b}" : $"{a} is not adjacent to {b}");
+}
+
+Coordinate c1 = new Coordinate { Row = 0, Column = 0 };
+Coordinate c2 = new Coordinate { Row = 1, Column = 1 };
+
+PrintAdjacency(c1, c2);
+
+Coordinate c3 = new Coordinate { Row = 2, Column = 1 };
+
+PrintAdjacency(c2, c3);
+
+Coordinate c4 = new Coordinate { Row = 0, Column = -1 };
+
+PrintAdjacency(c1, c4);
+
+/***** Type Definitions *****/
+
+public struct Coordinate
+{
+    public int Row { get; init; }
+    public int Column { get; init; }
+
+    public bool AdjacentTo(Coordinate other)
+    {
+        int rowDelta = Math.Abs(Row - other.Row);
+        int columnDelta = Math.Abs(Column - other.Column);
+
+        return (rowDelta + columnDelta) <= 1;
+    }
+
+    public override string ToString()
+    {
+        return $"({Column}, {Row})";
+    }
+}
