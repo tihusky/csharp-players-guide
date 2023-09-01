40235d8151c7d47520fbf7911887dd9f3d3e2338
Mirko Förster <mfoerster.hb@proton.me>
2023-09-01 11:36:49 +0200

    Finished level 29

---
+++ Level29/Level29/Program.cs
@@ -0,0 +1,15 @@
+﻿Point p1 = new Point(4, 2);
+Point p2 = new Point(4, 2);
+
+Console.WriteLine(p1 == p2);
+
+for (int x = (int)p1.X; x >= 0; x--)
+{
+    Point p3 = p1 with { X = x };
+
+    Console.WriteLine(p3);
+}
+
+/***** Type Definitions *****/
+
+public record Point(float X, float Y);
---
+++ Level29/WarPreparations/Program.cs
@@ -0,0 +1,30 @@
+﻿Sword basicSword = new Sword(Material.Iron, Gemstone.None, 85.0f, 24.0f);
+Sword trainingSword = basicSword with { Material = Material.Wood, Length = 65.0f, CrossguardWidth = 20.0f };
+Sword legendarySword = basicSword with { Material = Material.Binarium, Gemstone = Gemstone.Bitstone };
+
+Console.WriteLine(basicSword);
+Console.WriteLine(trainingSword);
+Console.WriteLine(legendarySword);
+
+/***** Type Definitions *****/
+
+public enum Material
+{
+    Wood,
+    Bronze,
+    Iron,
+    Steel,
+    Binarium
+}
+
+public enum Gemstone
+{
+    None,
+    Emerald,
+    Amber,
+    Sapphire,
+    Diamond,
+    Bitstone
+}
+
+public record Sword(Material Material, Gemstone Gemstone, float Length, float CrossguardWidth);
