bc72d9d53a1b1cf34b42a026073dc701546277a2
Mirko Förster <mfoerster.hb@proton.me>
2023-09-22 12:42:29 +0200

    Got started on level 37 and wrote some example code

---
+++ Level37/Level37/Program.cs
@@ -0,0 +1,42 @@
+﻿Ship ship = new();
+SoundEffectManager sem = new(ship);
+Random rng = new();
+
+while (!ship.IsDestroyed)
+    ship.TakeDamage(rng.Next(3, 12));
+
+/***** Type Definitions *****/
+
+public class Ship
+{
+    private int _health = 42;
+
+    public event Action? HasExploded;
+
+    public bool IsDestroyed => _health <= 0;
+
+    public void TakeDamage(int amount)
+    {
+        _health -= amount;
+
+        Console.WriteLine($"Ship took {amount} points of damage (Health: {_health})");
+
+        if (IsDestroyed)
+            HasExploded?.Invoke();
+    }
+}
+
+public class SoundEffectManager
+{
+    public SoundEffectManager(Ship ship)
+    {
+        ship.HasExploded += OnShipExploded;
+    }
+
+    public void OnShipExploded()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("KABOOOOOOOM!!!");
+        Console.ResetColor();
+    }
+}
