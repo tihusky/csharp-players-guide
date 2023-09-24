27e13fe88b83b8e2bddc9a7df588663c8ecf1757
Mirko Förster <mfoerster.hb@gmail.com>
2023-09-24 13:11:51 +0200

    Finished level 37

---
+++ Level37/CharberryTrees/Program.cs
@@ -0,0 +1,79 @@
+﻿var trees = new CharberryTree[] { new CharberryTree(), new CharberryTree() }; 
+var notifier = new Notifier(trees);
+var harvester = new Harvester(trees);
+
+while (true)
+{
+    foreach (CharberryTree tree in trees)
+        tree.MaybeGrow();
+}
+
+/***** Type Definitions *****/
+
+public class CharberryTree
+{
+    private static int NextId = 1;
+
+    private readonly Random _rng = new();
+
+    public int ID { get; }
+    public bool Ripe { get; set; } = false;
+
+    public event EventHandler? Ripened;
+
+    public CharberryTree()
+    {
+        ID = NextId++;
+    }
+
+    public void MaybeGrow()
+    {
+        if (_rng.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            Ripened?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
+
+public class Notifier
+{
+    public Notifier(params CharberryTree[] trees)
+    {
+        foreach (CharberryTree tree in trees)
+            tree.Ripened += OnRipened;
+    }
+
+    private void OnRipened(object? sender, EventArgs e)
+    {
+        if (sender is CharberryTree tree)
+        {
+            Console.Write($"[Tree #{tree.ID}] ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("A charberry fruit has ripened!");
+            Console.ResetColor();
+        }
+    }
+}
+
+public class Harvester
+{
+    public Harvester(params CharberryTree[] trees)
+    {
+        foreach (CharberryTree tree in trees)
+            tree.Ripened += OnRipened;
+    }
+
+    public void OnRipened(object? sender, EventArgs e)
+    {
+        if (sender is CharberryTree tree)
+        {
+            tree.Ripe = false;
+
+            Console.Write($"[Tree #{tree.ID}] ");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("A charberry fruit was harvested!");
+            Console.ResetColor();
+        }
+    }
+}
--- Level37/Level37/Program.cs
+++ Level37/Level37/Program.cs
@@ -1,17 +1,57 @@
-﻿Ship ship = new();
-SoundEffectManager sem = new(ship);
-Random rng = new();
+﻿Test3();
 
-while (!ship.IsDestroyed)
-    ship.TakeDamage(rng.Next(3, 12));
+void Test1()
+{
+    // Showing how to subscribe to events and that listeners exist as long as they're subscribed to an event
+    var ship = new Ship();
+    var effectManager = new ParticleEffectManager(ship);
+    var rng = new Random();
+
+    while (!ship.IsDestroyed)
+        ship.TakeDamage(rng.Next(3, 12));
+
+    effectManager = null; // No other references to the ParticleEffectManager instance
+
+    ship.TakeDamage(100);
+}
+
+void Test2()
+{
+    // Showing one possible solution to preventing event leaks
+    var ship = new Ship();
+    var effectManager = new ParticleEffectManager(ship);
+
+    // ParticleEffectManager still exists at this point and the event handler will be run
+    ship.TakeDamage(50);
+
+    // Get rid of the ParticleEffectManager; no listeners are attached to the HasExploded event anymore
+    effectManager.Cleanup();
+    effectManager = null;
+
+    ship.TakeDamage(50);
+}
+
+void Test3()
+{
+    // Testing the EventHandler delegate type and passing in the sender and arguments to the listener
+    var bomb = new Bomb(new Point(100, 250));
+    var balloon = new Balloon(new Point(275, 425));
+    var effectManager = new SoundEffectManager(bomb, balloon);
+
+    bomb.BlowUp();
+    balloon.BlowUp();
+}
 
 /***** Type Definitions *****/
+
+public record struct Point(float X, float Y);
 
 public class Ship
 {
     private int _health = 42;
+    private Point _location = new(24.7f, -193.4f);
 
-    public event Action? HasExploded;
+    public event Action<Point>? HasExploded;
 
     public bool IsDestroyed => _health <= 0;
 
@@ -22,21 +62,102 @@ public class Ship
         Console.WriteLine($"Ship took {amount} points of damage (Health: {_health})");
 
         if (IsDestroyed)
-            HasExploded?.Invoke();
+            HasExploded?.Invoke(_location);
+    }
+}
+
+public class BlowUpEventArgs : EventArgs
+{
+    public Point Location { get; }
+
+    public BlowUpEventArgs(Point location)
+    {
+        Location = location;
+    }
+}
+
+public class Bomb
+{
+    public Point Location { get; }
+
+    // i.e. void DoStuff(object sender, BlowUpEventArgs args)
+    public event EventHandler<BlowUpEventArgs>? IsBlownUp;
+
+    public Bomb(Point location)
+    {
+        Location = location;
+    }
+
+    public void BlowUp()
+    {
+        IsBlownUp?.Invoke(this, new BlowUpEventArgs(Location));
+    }
+}
+
+public class Balloon
+{
+    public Point Location { get; }
+
+    public event EventHandler<BlowUpEventArgs>? IsBlownUp;
+
+    public Balloon(Point location)
+    {
+        Location = location;
+    }
+
+    public void BlowUp()
+    {
+        IsBlownUp?.Invoke(this, new BlowUpEventArgs(Location));
+    }
+}
+
+public class ParticleEffectManager
+{
+    private Ship _ship;
+
+    public ParticleEffectManager(Ship ship)
+    {
+        _ship = ship;
+        _ship.HasExploded += OnShipExploded;
+    }
+
+    public void Cleanup()
+    {
+        _ship.HasExploded -= OnShipExploded;
+    }
+
+    private void OnShipExploded(Point location)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Create explosion effect at ({location.X}, {location.Y})");
+        Console.ResetColor();
     }
 }
 
 public class SoundEffectManager
 {
-    public SoundEffectManager(Ship ship)
+    public SoundEffectManager(Bomb bomb, Balloon balloon)
     {
-        ship.HasExploded += OnShipExploded;
+        bomb.IsBlownUp += OnBlowUp;
+        balloon.IsBlownUp += OnBlowUp;
     }
 
-    public void OnShipExploded()
+    public void OnBlowUp(object? sender, BlowUpEventArgs args)
     {
-        Console.ForegroundColor = ConsoleColor.DarkYellow;
-        Console.WriteLine("KABOOOOOOOM!!!");
-        Console.ResetColor();
+        Console.Write($"Sound Effect at ({args.Location.X}, {args.Location.Y}): ");
+
+        if (sender is Bomb)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("KABOOOOOOM!!!!!");
+            Console.ResetColor();
+        }
+
+        if (sender is Balloon)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("CHRRRRRRPT!!!!!");
+            Console.ResetColor();
+        }
     }
 }
