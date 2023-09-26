82105d67d0294cb55cc603a19055e90efea8b697
Mirko Förster <mfoerster.hb@gmail.com>
2023-09-26 16:57:19 +0200

    Finished level 39 up to 'File System Manipulation'

---
+++ Level39/Level39/Program.cs
@@ -0,0 +1,78 @@
+﻿if (!File.Exists("Scores.csv"))
+{
+    List<Score> scores = new()
+    {
+        new Score("Mirko", 19482, 17),
+        new Score("R2-D2", 12420, 15),
+        new Score("C3P0", 8543, 9),
+        new Score("GONK", -1, 1)
+    };
+
+    SaveScores(scores);
+}
+else
+{
+    List<Score> scores = LoadScores();
+
+    DisplayScores(scores);
+}
+
+void SaveScores(List<Score> scores)
+{
+    // Start with an empty file every time
+    File.Delete("Scores.csv");
+
+    StreamWriter writer = File.AppendText("Scores.csv");
+
+    foreach (Score s in scores)
+        writer.WriteLine(s.CsvString);
+
+    writer.Close();
+}
+
+List<Score> LoadScores()
+{
+    List<Score> scores = new();
+
+    if (!File.Exists("Scores.csv")) 
+        return scores;
+
+    string[] lines = File.ReadAllLines("Scores.csv");
+
+    foreach (string line in lines)
+    {
+        string[] tokens = line.Split(',');
+
+        scores.Add(new Score(
+            tokens[0],
+            Convert.ToInt32(tokens[1]),
+            Convert.ToInt32(tokens[2])
+        ));
+    }
+
+    return scores;
+}
+
+void DisplayScores(List<Score> scores)
+{
+    foreach (Score s in scores)
+        Console.WriteLine($"{s.Name, -8} {s.Points, 6} {s.Level, 3}");
+}
+
+/***** Type Definitions *****/
+
+public class Score
+{
+    public string Name { get; }
+    public int Points { get; }
+    public int Level { get; }
+
+    public Score(string name, int points, int level)
+    {
+        Name = name;
+        Points = points;
+        Level = level;
+    }
+
+    public string CsvString => $"{Name},{Points},{Level}";
+}
