329e5834c2dbd73b87d89d75cef8f30e1827bbd3
Mirko Förster <mfoerster.hb@proton.me>
2023-10-06 12:53:00 +0200

    Finished level 42 up to 'Grouping'

--- Level42/Level42/Program.cs
+++ Level42/Level42/Program.cs
@@ -1,4 +1,6 @@
-﻿var objects = new List<GameObject>()
+﻿using System.ComponentModel.DataAnnotations;
+
+var objects = new List<GameObject>()
 {
     new Ship() { ID = 4, X = 7, Y = 1, HP = 50, MaxHP = 100, PlayerID = 2 },
     new Ship() { ID = 1, X = 0, Y = 0, HP = 50, MaxHP = 100, PlayerID = 1 },
@@ -12,17 +14,56 @@ var players = new List<Player>()
     new Player(2, "Player 2", "Blue")
 };
 
-//var healthInfo = from o in objects
-//                 where o.HP > 50
-//                 where o.PlayerID == 1
-//                 select (o, $"{o.HP}/{o.MaxHP}");
+Test3();
 
-var healthInfo = objects
-                    .Where(o => o.HP > 0)
-                    .OrderBy(o => o.HP)
-                    .ThenBy(o => o.ID);
+void Test1()
+{
+    // It's not possible to access the Shield property in this way.
+    // The compiler only knows object contains GameObject instances.
+    // var shipStrings = from o in objects
+    //                   select (o, $"Shield: {o.Shield}, Hull: {o.HP}");
 
-Console.ReadKey();
+    // This code would throw an InvalidCastException if there's anything
+    // in objects that's not a ship.
+    // var shipStrings = from Ship s in objects
+    //                   select (s, $"Shield: {s.Shield}, Hull: {s.HP}");
+
+    // Alternative to the above using method call syntax. This would still
+    // throw an exception if there's anything other than a ship in the List.
+    // var shipStrings = objects
+    //                     .Cast<Ship>()
+    //                     .Select(s => (s, $"Shield: {s.Shield}, Hull: {s.HP}"));
+
+    // Alternative to the above. This filters out anything that's not a ship
+    // before casting.
+    var shipStrings = objects
+                        .OfType<Ship>()
+                        .Select(s => (s, $"Shield: {s.Shield}, Hull: {s.HP}"));
+}
+
+void Test2()
+{
+    var shipColors = from Ship s in objects
+                     join Player p in players on s.PlayerID equals p.ID
+                     select (s, p.TeamColor);
+}
+
+void Test3()
+{
+    // var playerOneDeadShips = from o in objects
+    //                          where o.PlayerID == 1
+    //                          select o
+    //                 into playerOneShip
+    //                          where playerOneShip.HP <= 0
+    //                          select playerOneShip;
+
+    var playerOneShips = from o in objects
+                         where o.PlayerID == 1
+                         select o;
+    var playerOneDeadShips = from s in playerOneShips
+                             where s.HP <= 0
+                             select s;
+}
 
 /***** Type Definitions *****/
 
@@ -36,6 +77,9 @@ public class GameObject
     public int PlayerID { get; set; }
 }
 
-public class Ship : GameObject { }
+public class Ship : GameObject 
+{
+    public int Shield { get; set; }
+}
 
 public record Player(int ID, string UserName, string TeamColor);
