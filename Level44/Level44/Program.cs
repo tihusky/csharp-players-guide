8a0d20b038a9f67292451b20e2868d29bc1817aa
Mirko Förster <mfoerster.hb@gmail.com>
2023-10-24 17:17:18 +0200

    Read chapter 44 up to 'Who Runs My Code?'

---
+++ Level44/Level44/Program.cs
@@ -0,0 +1,21 @@
+﻿var computationResult = await DoWork();
+
+Console.WriteLine(computationResult);
+
+Task<int> AddOnEuropa(int a, int b)
+{
+    return Task.Run(() =>
+    {
+        Thread.Sleep(3000);
+        return a + b;
+    });
+}
+
+async Task<int> DoWork()
+{
+    var res1 = await AddOnEuropa(2, 3);
+    var res2 = await AddOnEuropa(4, 5);
+    var res3 = await AddOnEuropa(res1, res2);
+
+    return res3;
+}
